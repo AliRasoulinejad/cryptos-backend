3c264d546b057af321e621d8e56cafb7463d4be8
ali <ali.rasoulinejad75@gmail.com>
2023-01-25 22:55:03 +0330

    add url as span name

--- internal/http/middlewares/tracing.go
+++ internal/http/middlewares/tracing.go
@@ -10,7 +10,7 @@ import (
 func TracingMiddleware(tracer trace.Tracer) func(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			spanCtx, span := tracer.Start(c.Request().Context(), "tracing-middleware")
+			spanCtx, span := tracer.Start(c.Request().Context(), c.Request().URL.Path)
 			c.Set(app.SpanCtxName, spanCtx)
 			result := next(c)
 			span.End()
